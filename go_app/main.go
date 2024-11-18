3bb4ee4c2284f39ff5b524476fa65356428e485c
Shady Zekry <shadyzekry75@gmail.com>
2024-11-18 04:43:52 +0200

    Add rabbitmq as a chat queue on chat creation

--- go_app/main.go
+++ go_app/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/ShadyZekry/chat-app/services"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Chat struct {
@@ -19,8 +22,6 @@ type Message struct {
 	Name   string `json:"name,omitempty"`
 }
 
-var chats []Chat
-
 func createChat(c echo.Context) error {
 	chat := new(Chat)
 	chat.Name = c.FormValue("name")
@@ -28,8 +29,48 @@ func createChat(c echo.Context) error {
 	token := c.Param("token")
 	chat.Number = services.Increment(token)
 
-	chats = append(chats, *chat)
-	return c.JSON(http.StatusCreated, chats)
+	publishChat(*chat)
+	return c.JSON(http.StatusCreated, chat)
+}
+
+func publishChat(chat Chat) {
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"chats", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body, err := json.Marshal(chat)
+	failOnError(err, "Failed to marshal chat")
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		})
+	failOnError(err, "Failed to publish a message")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
 }
 
 func main() {
